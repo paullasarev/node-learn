251e8617110932992e09a39ddded04869f98d34e
paullasarev <paullasarev@gmail.com>
2017-11-12 14:42:38 +0400

    fix upper

--- app/utils/upper-case.js
+++ app/utils/upper-case.js
@@ -1,19 +1,19 @@
 import through2 from 'through2';
 import { autobind } from 'core-decorators';
 
+function upper(chunk, enc, callback) {
+  this.push(chunk.toString().toUpperCase())
+  callback()
+}
+
 export class UpperCase {
   constructor(argv) {
   }
 
   run() {
     process.stdin
-      .pipe(through2(this.upper))
+      .pipe(through2(upper))
       .pipe(process.stdout)
   }
 
-  @autobind
-  upper(pipe, chunk, enc, callback) {
-    pipe.push(chunk.toString().toUpperCase())
-    callback()
-  }
 }
