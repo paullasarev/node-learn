06e5ffa4c7a46eddfabf27f0854a8d9e4518c1b0
paullasarev <paullasarev@gmail.com>
2017-11-13 12:03:31 +0400

    route

--- app/index.js
+++ app/index.js
@@ -10,23 +10,26 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 console.log("app name:", config.App.name);
-const user = new User();
-const product = new Product();
+// const user = new User();
+// const product = new Product();
 
-const imp = new Importer();
-const res = imp.importSync('./data');
-console.log(res);
+// const imp = new Importer();
+// const res = imp.importSync('./data');
+// console.log(res);
 
-imp.watch('./data').then((res)=>{
-  console.log(res)
-})
+// imp.watch('./data').then((res)=>{
+//   console.log(res)
+// })
 
 const app = express();
 app.use(morgan('tiny'));
 
-app.get('/', (req, res) => {
+const router = express.Router();
+router.get('/', (req, res) => {
   res.send('Hello world\n');
 });
+
+app.use('/', router)
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
