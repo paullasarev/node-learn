210fb18aa9963a5e23467b10e4b8fd00a027f375
paullasarev <paullasarev@gmail.com>
2017-11-14 22:03:05 +0400

    sequelizer

--- app/models/product.js
+++ app/models/product.js
@@ -1,8 +1,17 @@
-
-class Product {
-  constructor() {
-    console.log('Product module');
-  }
-}
-
-module.exports = Product;
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    name: {
+    type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    price: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+    },
+  });
+  return Product;
+};
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,21 @@
-
-class User {
-  constructor() {
-    console.log('User module');
-  }
-}
-
-module.exports = User;
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+    type: DataTypes.STRING,
+      allowNull: false,
+    },
+    login: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return User;
+};
